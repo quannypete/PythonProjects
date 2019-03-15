e6a8bbb3fa76aa217559bf8a01a83668c32f9709
Quanny <younghampton@gmail.com>
2019-03-15 18:09:23 -0400

    update to shoot the app

--- shoot_the_fruit/shoot.py
+++ shoot_the_fruit/shoot.py
@@ -1,18 +1,21 @@
 from random import randint
 
+score = 0
 apple= Actor("apple")
-
 def draw():
     screen.clear()
     apple.draw()
+    screen.draw.text("Score: " + str(score),color = "white", topleft = (10,10))
 
 def place_apple():
     apple.x = randint(10,800)
     apple.y = randint(10,600)
 
 def on_mouse_down(pos):
+    global score
     if apple.collidepoint(pos):
         print("Good shot!")
+        score = score +1
         place_apple()
     else:
         print("You missed!")
